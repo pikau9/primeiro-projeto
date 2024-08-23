9c4c90bc534498f307649e64d7fd813730b97cba
pikau9 <pietro.oliveira.antunes@gmail.com>
2024-08-22 21:42:31 -0300

    Add files via upload

--- novo-projeto-de-jogo/node_2d.gd
+++ novo-projeto-de-jogo/node_2d.gd
@@ -3,42 +3,40 @@ extends Node2D
 @export var bug_scene : PackedScene
 var score = 0
 
-
 func _ready():
 	new_game()
 
 func game_over():
 	$BugTimer.stop()
 	$ScoreTimer.stop()
-	
+	# Adicione outras operações de final de jogo, se necessário
+
 func new_game():
 	$StartTimer.start()
 	$player.start_pos($StartPosition.position)
 	score = 0
-	
+
 func _on_score_timer_timeout():
 	score += 1
 
 func _on_start_timer_timeout():
 	$BugTimer.start()
 	$ScoreTimer.start()
-	
+
 func _on_bug_timer_timeout():
 	var bug = bug_scene.instantiate()
 	var bug_location = $BugPath/BugPathLocation
-	bug_location.progress_ratio = randf()
-	
-	
+	bug.position = bug_location.position  # Define a posição do bug
+
 	var direction = bug_location.rotation + PI / 2
-	bug.position - bug_location.position
-	direction += randf_range(-PI /4, PI /4)
-	bug.rotation = direction 
-	
-	var velocity =Vector2(randf_range(150.0,250.0), 0.0)
-	bug.linear_velocity = velocity.rotated(direction)
+	direction += randf_range(-PI / 4, PI / 4)
+	bug.rotation = direction
+
+	if bug is RigidBody2D:
+		var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+		bug.linear_velocity = velocity.rotated(direction)
+	else:
+		# Se bug não for um RigidBody2D, você precisa definir a lógica de movimento de outra forma
+		pass
+
 	add_child(bug)
-	
-	
-	
-	
-	
